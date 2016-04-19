9ad620537855186796b64bc2dd53cf3a080a09dc
rangertaha <tahasaidi@gmail.com>
2016-04-18 22:17:19 -0400

    updates

--- example/simpleapp/admin.py
+++ example/simpleapp/admin.py
@@ -1,12 +1,2 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-
-from .models import SimpleModel
-
-
-class SimpleModelAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    prepopulated_fields = {'slug': ('name',)}
-    search_fields = ('name',)
-
-admin.site.register(SimpleModel, SimpleModelAdmin)
--- example/simpleapp/forms.py
+++ example/simpleapp/forms.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
 from django import forms
 
-
-class ExtraMetaForm(forms.Form):
-    num = forms.IntegerField()
-    choice = forms.ChoiceField(choices=((1, 'male'), (2, 'female')),
-                               required=False)
